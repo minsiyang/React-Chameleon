ff69b302c92e1095d761b0812eb9c24b0607930a
Minsi Yang <yangminsi@gmail.com>
2020-09-14 16:38:55 +0100

    Add heading component

---
+++ src/Heading.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Heading(props) {
+  return (
+    <div className="heading" style={{color: props.color}}>
+      I am a Chameleon
+    </div>
+  )
+}
+
+export default Heading;
